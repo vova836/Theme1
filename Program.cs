22b17c846025d1908612083214018e970c11ed4d
vova836 <123488703+vova836@users.noreply.github.com>
2024-02-04 10:58:03 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using CSharpShellCore;
+
+namespace К.П_1;
+
+public static class Program
+{
+	public static void Main()
+	{
+     //Задание 1. Вывести сообщение с информацией о текущей системной дате, применив несколько форматов.
+	 //Текущая дата
+	 //DateTime date = DateTime.Now;
+	 //Console.WriteLine($"d: {date.ToString("d")}");
+	 //Console.WriteLine($"D: {date.ToString("D")}");
+	 //Console.WriteLine(date.ToString("dd MMM yyyy"));
+	 //Console.WriteLine(date.ToString("ddd, dd MMM yyyy"));
+	 //Console.WriteLine(date.ToString("dddd, dd MMMM yyyy"));
+	 
+	 //Текущая время
+	 //Console.WriteLine($"t: {date:t}");
+	 //Console.WriteLine($"T: {date:T}");
+	 
+	 //Текущая дата и время
+	 //Console.WriteLine($"F: {date.ToString("F")}");
+	 //Console.WriteLine($"u: {date:u}");
+	 //Console.WriteLine($"R: {date:R}");
+	 //Console.WriteLine($"U: {date:U}");
+	 
+	 //Задание 2. Запросить с клавиатуры возраст человека и вывести на экран следующее сообщение: Ты родился в .... году
+	 //Console.WriteLine("Введите свой возраст");
+	 //int age = Convert.ToInt32(Console.ReadLine());
+	 //DateTime date1 = DateTime.Now;
+	 //Console.WriteLine(date1.ToString("23.01.2006"));
+	 
+	 //Console.ReadKey();
+	}
+}
